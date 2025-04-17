8dcc6d73ec504b0bc8bef13a73f43c6546c30a3f
Gabriel Hoppe <gabrielh2c@hotmail.com>
2025-04-17 09:03:01 -0300

    Initial Commit

---
+++ almaden-agent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  almaden-agent
+//
+//  Created by Gabriel Hoppe on 17/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ almaden-agent/almaden_agentApp.swift
@@ -0,0 +1,17 @@
+//
+//  almaden_agentApp.swift
+//  almaden-agent
+//
+//  Created by Gabriel Hoppe on 17/04/25.
+//
+
+import SwiftUI
+
+@main
+struct almaden_agentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
